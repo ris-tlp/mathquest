2bea2b93f4fa14a5d92d6cd406c5ea50e7b25ea6
ris-tlp <omarkhantlp@gmail.com>
2023-11-21 19:35:12 -0500

    add fetch by course id endpoint

--- server/src/course/courseController.ts
+++ server/src/course/courseController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { Course } from "../models/courseModel";
+import { Course } from "./courseModel";
 
 export const courseRouter = Router();
 
@@ -16,6 +16,30 @@ courseRouter.post("/", async (req: Request, res: Response) => {
             });
         } catch (error) {
             res.status(400).json({ error: "Invalid request body." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+courseRouter.get("/:courseId", async (req: Request, res: Response) => {
+    try {
+        const params = req.params;
+
+        const queryResult = await Course.findOne({
+            _id: params["courseId"],
+        })
+            .select("-isPublished -__v")
+            .exec();
+
+        if (!queryResult) {
+            res.status(404).json({
+                error: "Course not found.",
+            });
+        } else {
+            res.status(200).json({
+                queryResult,
+            });
         }
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
--- server/src/user/userController.ts
+++ server/src/user/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { User } from "../models/userModel";
+import { User } from "./userModel";
 
 export const userRouter = Router();
 
