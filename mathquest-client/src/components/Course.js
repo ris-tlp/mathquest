9491fafd9e0fdeb45251d1ada7bb31c22cb19283
Anand,Vermam <anandverma552@outlook.comm>
2023-11-22 20:28:01 -0500

    added course page

--- mathquest-client/src/components/Course.js
+++ mathquest-client/src/components/Course.js
@@ -1,22 +1,58 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import { Tab, Tabs } from "./Tabs";
+import Overview from "./Overview";
+import Discussion from "./Discussion";
+import Announcements from "./Announcements";
+
+
 
 const Course = () => {
   return (
     <div>
+      <Header />
 
-        <Header />
+      <div className="pt-40 mx-10 w-8/12 sm:w-[100%]">
+        {/* <iframe
+          className="h-[600px]"
+          width="100%"
+          src="https://www.youtube.com/watch?v=5zi5eG5Ui-Y?controls=1"
+        ></iframe> */}
 
+        <iframe
+         className="h-[600px]"
+          width="100%"
+          src="https://www.youtube.com/embed/5zi5eG5Ui-Y?si=PbLAE9ieALFQ7izX"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
 
-        <div>
-
-
-
+      <div>
+        <Tabs>
+          <Tab label="Overview">
+            <div className="py-4">
+              <Overview />
             
-        </div>
 
+            </div>
+          </Tab>
+          <Tab label="Discussion">
+            <div className="py-4">
+              <Discussion />
+            </div>
+          </Tab>
+          <Tab label="Announcements">
+            <div className="py-4">
+              <Announcements />
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
