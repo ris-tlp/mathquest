bb1bc794764b956cd4936e381cd2148e6da05fc2
Anand,Vermam <anandverma552@outlook.comm>
2023-11-22 23:32:32 -0500

    added profile

--- mathquest-client/src/components/Profile.js
+++ mathquest-client/src/components/Profile.js
@@ -11,25 +11,52 @@ const Profile = () => {
       <Header />
       <div className="pt-40"></div>
 
-      {
-        user && <div class="w-full mx-20 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
-        <div class="flex flex-col items-center p-10">
-          <img
-            class="w-24 h-24 mb-3 rounded-full shadow-lg"
-            alt="Bonnie image"
-            src={user?.photoURL}
-          />
-          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-            { user?.displayName }
-          </h5>
-          
-          
+      {user && (
+        <div class="bg-slate-300 max-w-2xl shadow overflow-hidden sm:rounded-lg m-auto ">
+          <div class="px-4 py-5 sm:px-6 text-center">
+            <h1 class="text-2xl leading-6 font-medium slate-900">
+              User Profile
+            </h1>
+            <p class="mt-1 max-w-2xl text-sm slate-900 text-xl">
+              Details and informations about {user?.displayName}
+            </p>
+          </div>
+          <div class="border-t border-gray-200 text-center">
+            <dl>
+              <div class="bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 text-center">
+                <img className="rounded-full h-[150px] text-center m-auto" src={user?.photoURL}></img>
+              </div>
+              <div class="bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-lg font-medium text-gray-500">Full name</dt>
+                <dd class="mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2">
+                  {user?.displayName}
+                </dd>
+              </div>
+              <div class="bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-lg font-medium text-gray-500">Skills</dt>
+                <dd class="mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2">
+                  React JS
+                </dd>
+              </div>
+              <div class="bg-gray-100 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-lg font-medium text-gray-500">Email address</dt>
+                <dd class="mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2">
+                 {user?.email}
+                </dd>
+              </div>
+             
+              <div class="bg-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-lg font-medium text-gray-500">About</dt>
+                <dd class="mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2">
+                  To get social media testimonials like these, keep your
+                  customers engaged with your social media accounts by posting
+                  regularly yourself
+                </dd>
+              </div>
+            </dl>
+          </div>
         </div>
-      </div>
-      }
-
-      
+      )}
     </div>
   );
 };
