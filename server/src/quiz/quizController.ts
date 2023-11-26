3ab5d1782634ed8d3e6e8421b201d9d323c1137c
ris-tlp <omarkhantlp@gmail.com>
2023-11-26 13:03:09 -0500

    number of questions returned with getAllQuizzes

--- server/src/quiz/quizController.ts
+++ server/src/quiz/quizController.ts
@@ -16,9 +16,26 @@ quizRouter.post("/getAllQuizzes", async (req: Request, res: Response) => {
             courseID: courseID,
         }).exec();
 
-        res.status(200)
-            .setHeader("Content-Type", "application/json")
-            .json({ result: queryResult });
+        if (queryResult) {
+            let finalizedQuizzes = [];
+
+            for (let i = 0; i < queryResult.length; i++) {
+                let quiz = JSON.stringify(queryResult[i]);
+                let quizJson = JSON.parse(quiz);
+
+                const numberOfQuestions = await QuizQuestion.countDocuments({
+                    quizID: queryResult[i]._id,
+                }).exec();
+
+                quizJson.numberOfQuestions = numberOfQuestions;
+
+                finalizedQuizzes.push(quizJson);
+            }
+
+            res.status(200)
+                .setHeader("Content-Type", "application/json")
+                .json({ result: finalizedQuizzes });
+        }
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
     }
