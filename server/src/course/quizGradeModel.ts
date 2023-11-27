79a73a6afd24537010081d43d1992d2f62a15bb6
ris-tlp <omarkhantlp@gmail.com>
2023-11-26 19:29:48 -0500

    remove attempts

--- server/src/course/quizGradeModel.ts
+++ server/src/course/quizGradeModel.ts
@@ -4,14 +4,12 @@ interface IQuizGrade {
     userID: string;
     quizID: Types.ObjectId;
     score: number;
-    attempts: number;
 }
 
 const quizGradeSchema = new Schema<IQuizGrade>({
     userID: { type: String, required: true },
     quizID: { type: Schema.Types.ObjectId, required: true, ref: "Quiz" },
     score: { type: Number, required: true },
-    attempts: { type: Number, required: true },
 });
 
 export const QuizGrade = model<IQuizGrade>("QuizGrade", quizGradeSchema);
