78ac92024d4991619c5dd2b1b6b84f2e36e63be5
ris-tlp <omarkhantlp@gmail.com>
2023-11-26 19:31:55 -0500

    change userID to email

--- server/src/course/quizGradeModel.ts
+++ server/src/course/quizGradeModel.ts
@@ -1,13 +1,13 @@
 import { Schema, Types, model } from "mongoose";
 
 interface IQuizGrade {
-    userID: string;
+    email: string;
     quizID: Types.ObjectId;
     score: number;
 }
 
 const quizGradeSchema = new Schema<IQuizGrade>({
-    userID: { type: String, required: true },
+    email: { type: String, required: true },
     quizID: { type: Schema.Types.ObjectId, required: true, ref: "Quiz" },
     score: { type: Number, required: true },
 });
