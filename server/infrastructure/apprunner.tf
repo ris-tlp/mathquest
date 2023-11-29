acc0b2aff11fab99553b30ccafff56a3c6ef5acc
ris-tlp <omarkhantlp@gmail.com>
2023-11-29 06:12:38 -0500

    apprunner config

--- server/infrastructure/apprunner.tf
+++ server/infrastructure/apprunner.tf
@@ -1,24 +1,27 @@
-# resource "aws_apprunner_service" "mathquest-api" {
-#   service_name = "mathquest-api"
+resource "aws_apprunner_service" "mathquest-api" {
+  service_name = "mathquest-api"
 
-#   source_configuration {
+  source_configuration {
+    # authentication_configuration {
+    #   access_role_arn = aws_iam_role.access_role.arn
+    # }
 
-#     image_repository {
-#       image_configuration {
-#         port = "8000"
-#       }
-#       image_identifier      = aws_ecrpublic_repository.mathquest-docker-repo.repository_uri
-#       image_repository_type = "ECR_PUBLIC"
-#     }
-#     auto_deployments_enabled = false
-#   }
+    image_repository {
+      image_configuration {
+        port = "8000"
+      }
+      image_identifier      = "${aws_ecr_repository.mathquest-docker-repo.repository_url}:latest"
+      image_repository_type = "ECR_PUBLIC"
+    }
+    auto_deployments_enabled = false
+  }
 
-#   instance_configuration {
-#     cpu    = 1024
-#     memory = 2048
-#   }
+  instance_configuration {
+    cpu    = 1024
+    memory = 2048
+  }
 
-#   tags = {
-#     Name = "mathquest-api"
-#   }
-# }
+  tags = {
+    Name = "mathquest-api"
+  }
+}
