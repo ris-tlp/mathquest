20b044c914fa533b883b28ed0f71fd703949b3b0
ris-tlp <omarkhantlp@gmail.com>
2023-11-29 02:10:59 -0500

    remove skills from user profile

--- server/src/user/userController.ts
+++ server/src/user/userController.ts
@@ -37,7 +37,7 @@ userRouter.get("/type", async (req: Request, res: Response) => {
     }
 });
 
-// Create
+// Add a set of skills to your profile
 userRouter.post("/addProfileSkills", async (req: Request, res: Response) => {
     try {
         // in the form of [skill1, skill2]
@@ -56,6 +56,30 @@ userRouter.post("/addProfileSkills", async (req: Request, res: Response) => {
             .setHeader("Content-Type", "application/json")
             .json({ result: user });
     } catch (error) {
+        res.status(500).json({ error: "Internal Server Error." });
+    }
+});
+
+// Remove skills from profile
+userRouter.post("/deleteProfileSkills", async (req: Request, res: Response) => {
+    try {
+        // in the form of [skill1, skill2]
+        const skills = req.body.skills;
+        const email = req.body.email;
+
+        const updateResult = await User.updateOne(
+            { email: email },
+            { $pull: { skills: { $in: skills } } },
+            { upsert: true }
+        ).exec();
+
+        const user = await User.findOne({ email: email }).exec();
+
+        res.status(201)
+            .setHeader("Content-Type", "application/json")
+            .json({ result: user });
+    } catch (error) {
+        console.log(error);
         res.status(500).json({ error: "Internal Server Error." });
     }
 });
