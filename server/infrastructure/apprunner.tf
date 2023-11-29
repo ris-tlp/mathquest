8e00d31b8b6df7f1a3cc1ebb349ef4510875f7f5
ris-tlp <omarkhantlp@gmail.com>
2023-11-29 05:58:15 -0500

    one more time

--- server/infrastructure/apprunner.tf
+++ server/infrastructure/apprunner.tf
@@ -1,24 +1,24 @@
-resource "aws_apprunner_service" "mathquest-api" {
-  service_name = "mathquest-api"
+# resource "aws_apprunner_service" "mathquest-api" {
+#   service_name = "mathquest-api"
 
-  source_configuration {
+#   source_configuration {
 
-    image_repository {
-      image_configuration {
-        port = "8000"
-      }
-      image_identifier      = aws_ecrpublic_repository.mathquest-docker-repo.repository_uri
-      image_repository_type = "ECR_PUBLIC"
-    }
-    auto_deployments_enabled = false
-  }
+#     image_repository {
+#       image_configuration {
+#         port = "8000"
+#       }
+#       image_identifier      = aws_ecrpublic_repository.mathquest-docker-repo.repository_uri
+#       image_repository_type = "ECR_PUBLIC"
+#     }
+#     auto_deployments_enabled = false
+#   }
 
-  instance_configuration {
-    cpu    = 1024
-    memory = 2048
-  }
+#   instance_configuration {
+#     cpu    = 1024
+#     memory = 2048
+#   }
 
-  tags = {
-    Name = "mathquest-api"
-  }
-}
+#   tags = {
+#     Name = "mathquest-api"
+#   }
+# }
--- server/infrastructure/ecr.tf
+++ server/infrastructure/ecr.tf
@@ -1,9 +1,4 @@
-resource "aws_ecrpublic_repository" "mathquest-docker-repo" {
+resource "aws_ecr_repository" "mathquest-docker-repo" {
   #   provider = aws.us_east_1
-
-  repository_name = "mathquest-docker-repo"
-
-  catalog_data {
-    about_text = "All docker images involved with the mathquest api."
-  }
+  name = "mathquest-docker-repo"
 }
