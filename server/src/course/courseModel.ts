88c9951ff6e3f3f57b836f1e49df8b73e8a5cca5
ris-tlp <omarkhantlp@gmail.com>
2023-11-29 01:21:32 -0500

    update model to match mongo

--- server/src/course/courseModel.ts
+++ server/src/course/courseModel.ts
@@ -6,16 +6,32 @@ interface ICourse {
     courseSyllabus: string;
     passScore: number;
     isPublished: boolean;
-    courseImage: string;
+    courseImg: string;
+    courseInstructor: string;
+    email: string; // important
+    courseDuration: string;
+    overview: string;
+    instructorImage: string;
+    instructorDescription: string;
+    whatYouWillLearn: [string];
+    courseVideoUrl: string;
 }
 
 const courseSchema = new Schema<ICourse>({
     courseName: { type: String, required: true, unique: true },
     courseDescription: { type: String, required: false },
     courseSyllabus: { type: String, required: false },
-    courseImage: { type: String },
+    courseImg: { type: String },
     passScore: { type: Number, required: false },
     isPublished: { type: Boolean, required: false },
+    courseInstructor: { type: String, required: false },
+    email: { type: String, required: true },
+    courseDuration: { type: String, required: false },
+    overview: { type: String, required: false },
+    instructorImage: { type: String, required: false },
+    instructorDescription: { type: String, required: false },
+    whatYouWillLearn: [{ type: String, required: false }],
+    courseVideoUrl: { type: String, required: false },
 });
 
 export const Course = model<ICourse>("Course", courseSchema);
